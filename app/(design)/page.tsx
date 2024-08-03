2bec714caa8edb179123fc27ad08c366824bbd68
Sebastian Stoelen <gewoonseba@gmail.com>
2024-08-03 09:36:01 +0200

    tweaking row

--- app/(design)/page.tsx
+++ app/(design)/page.tsx
@@ -4,34 +4,44 @@ import Link from "next/link";
 export default function Design() {
   return (
     <div>
-      <section className="grid place-items-center ~py-8/20">
+      <section className="grid place-items-center ~py-4/10">
         <h1>Gewoon Seba.</h1>
       </section>
-      <section className="~py-4/10">
-        <p className="max-w-prose whitespace-pre-line">
+      <section className="flex max-w-prose flex-col gap-2 ~py-4/10">
+        <p>
           Hello! I'm Seba, a designer with a soft spot for code. Currently, I’m
           leading product at{" "}
           <Link
-            className="transition-all duration-150 hover:text-neutral-100 hover:underline"
+            className="whitespace-nowrap transition-all duration-150 hover:text-neutral-100 hover:underline"
             href="https://www.smooved.be"
           >
             Smooved ↗
           </Link>
-          . I believe that details matter, form is also a function and an
-          excellent product sells itself. I’m driven by curiosity and a desire
-          to continually improve my craft.<br></br> Nice to meet you.
+          .{" "}
         </p>
+        <p>
+          I believe that details matter, form is also a function and an
+          excellent product sells itself. I’m driven by curiosity and a desire
+          to continually improve my craft.
+        </p>
+        <p>Nice to meet you.</p>
       </section>
       <section className="~py-4/10">
         <h2 className="pb-4">Experience</h2>
         <LinkRow className="border-t" header="Head of Product" byline="Smooved">
-          <span className="uppercase text-neutral-100">current</span>
+          <span className="uppercase tracking-tight text-neutral-100 ~text-sm/base">
+            current
+          </span>
         </LinkRow>
         <LinkRow header="Product Designer" byline="In The Pocket">
-          <span className="uppercase text-neutral-100">2021 - 2023</span>
+          <span className="uppercase tracking-tight text-neutral-100 ~text-sm/base">
+            2021 - 2023
+          </span>
         </LinkRow>
         <LinkRow header="Full stack engineer" byline="AE">
-          <span className="uppercase text-neutral-100">2017 - 2021</span>
+          <span className="uppercase tracking-tight text-neutral-100 ~text-sm/base">
+            2017 - 2021
+          </span>
         </LinkRow>
       </section>
       <section className="~py-4/10">
@@ -43,25 +53,25 @@ export default function Design() {
         >
           <Link
             href="https://www.inthepocket.com/work/izimi"
-            className="uppercase text-neutral-100 transition-all duration-150 hover:text-neutral-100 hover:underline"
+            className="uppercase tracking-tight text-neutral-100 transition-all duration-150 ~text-sm/base hover:text-neutral-100 hover:underline"
           >
-            inthepocket.com ↗
+            view case ↗
           </Link>
         </LinkRow>
         <LinkRow header="TP Vision Wave" byline="Digital Product Design">
           <Link
             href="https://www.inthepocket.com/work/tp-vision"
-            className="uppercase text-neutral-100 transition-all duration-150 hover:text-neutral-100 hover:underline"
+            className="uppercase tracking-tight text-neutral-100 transition-all duration-150 ~text-sm/base hover:text-neutral-100 hover:underline"
           >
-            inthepocket.com ↗
+            view case ↗
           </Link>
         </LinkRow>
         <LinkRow header="Multipharma" byline="Digital Product Design">
           <Link
             href="https://www.inthepocket.com/work/multipharma"
-            className="uppercase text-neutral-100 transition-all duration-150 hover:text-neutral-100 hover:underline"
+            className="uppercase tracking-tight text-neutral-100 transition-all duration-150 ~text-sm/base hover:text-neutral-100 hover:underline"
           >
-            inthepocket.com ↗
+            view case ↗
           </Link>
         </LinkRow>
       </section>
--- app/ui/link-row.tsx
+++ app/ui/link-row.tsx
@@ -12,7 +12,7 @@ export function LinkRow({ className, header, byline, children }: LinkRowProps) {
   return (
     <div
       className={classNames(
-        "flex flex-col justify-between border-b border-neutral-100/5 py-3 md:flex-row md:items-center",
+        "flex items-center justify-between border-b border-neutral-100/5 py-4",
         className,
       )}
     >
@@ -20,7 +20,7 @@ export function LinkRow({ className, header, byline, children }: LinkRowProps) {
         <h3>{header}</h3>
         <p className="uppercase ~text-sm/base">{byline}</p>
       </div>
-      <div className="mt-4 flex-shrink-0 md:ml-4 md:mt-0">{children}</div>
+      <div className="ml-4 flex-shrink-0">{children}</div>
     </div>
   );
 }
