dbe994475c084d046c50a0a68c86ba6444601090
Sebastian Stoelen <gewoonseba@gmail.com>
2024-08-05 19:42:27 +0200

    added dummy photo grid

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,3 +1,98 @@
 export default function Photos() {
-    return <div>Photos</div>;
+  return (
+    <section className="grid grid-cols-2 gap-4 ~py-4/10 md:grid-cols-4">
+      <div className="grid gap-4">
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
+            alt=""
+          ></img>
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
+            alt=""
+          ></img>
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
+            alt=""
+          ></img>
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-lg h-auto max-w-full"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg"
+            alt=""
+          ></img>
+        </div>
+      </div>
+    </section>
+  );
 }
