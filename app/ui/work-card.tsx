4e5831b19b62371263146a3a3092876de58d8d03
Sebastian Stoelen <gewoonseba@gmail.com>
2024-09-14 13:50:40 +0200

    wip make cards paramters

--- app/ui/work-card.tsx
+++ app/ui/work-card.tsx
@@ -1,7 +1,14 @@
+import { LinkButton } from "@/app/ui/button/link-button";
 import multipharma from "@/public/img/design/multipharma.jpg";
 import Image from "next/image";
 
-export function WorkCard() {
+interface WorkCardProps {
+  title: string;
+  byline: string;
+  link?: string;
+}
+
+export function WorkCard({ title, byline, link }: WorkCardProps) {
   return (
     <div>
       <Image
@@ -10,8 +17,11 @@ export function WorkCard() {
         alt=""
         placeholder="blur"
       ></Image>
-      <h4 className="mt-2">Multipharma</h4>
-      <p className="~text-sm/base">2022 · In The Pocket</p>
+      <div className="flex justify-between">
+        <h4 className="mt-2">{title}</h4>
+        {link && <LinkButton label="Read case" url={link} />}
+      </div>
+      <p className="~text-sm/base">{byline}</p>
     </div>
   );
 }
