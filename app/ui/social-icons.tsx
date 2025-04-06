3fd1d06609ed93309bfd658cb9e6f4c4f05d3be5
Sebastian Stoelen <gewoonseba@gmail.com>
2025-04-06 20:00:01 +0200

    Refactor styles and update social icons
    * Adjusted CSS utility classes for headings and paragraphs for improved consistency.
    * Replaced Instagram and ReadCV icons with Bluesky and Threads in the social links component.

--- app/ui/social-icons.tsx
+++ app/ui/social-icons.tsx
@@ -1,17 +1,17 @@
 import { IconLink } from "@/app/ui/button/icon-link";
-import { Instagram } from "@/app/ui/icons/instagram";
+import { Bluesky } from "@/app/ui/icons/bluesky";
 import { LinkedIn } from "@/app/ui/icons/linkedin";
-import { ReadCV } from "@/app/ui/icons/readcv";
+import { Threads } from "@/app/ui/icons/threads";
 import classNames from "classnames";
 
 export const SocialLinks = ({ className }: { className?: string }) => {
   return (
     <div className={classNames("flex gap-1", className)}>
-      <IconLink href="https://www.instagram.com/gewoon.seba/">
-        <Instagram />
+      <IconLink href="https://www.threads.net/@gewoon.seba">
+        <Threads />
       </IconLink>
-      <IconLink href="https://read.cv/gewoon.seba">
-        <ReadCV />
+      <IconLink href="https://bsky.app/profile/gewoonseba.com">
+        <Bluesky />
       </IconLink>
       <IconLink href="https://www.linkedin.com/in/sebastian-stoelen-43b605b4/">
         <LinkedIn />
