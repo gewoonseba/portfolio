499728af5ba92ddad71c9eed836fdf7169a6e17d
Sebastian Stoelen <gewoonseba@gmail.com>
2025-06-07 22:03:20 +0200

    Add atuosized text

--- app/components/typography/hero.tsx
+++ app/components/typography/hero.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { AutoSizedText } from "../auto-sized-text";
+
 export default function Hero() {
-  return <h1 className="text-huge font-semibold tracking-tighter text-primary">
-    Gewoon Seba.
-  </h1>;
+  return (
+    <div className="w-full">
+      <AutoSizedText>
+        <h1 className="text-primary leading-none font-semibold tracking-tighter whitespace-nowrap">
+          Gewoon Seba.
+        </h1>
+      </AutoSizedText>
+    </div>
+  );
 }
