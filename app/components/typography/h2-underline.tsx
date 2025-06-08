dc817b31247df982f956d0df65962be1e7381cb9
Sebastian Stoelen <gewoonseba@gmail.com>
2025-06-08 08:43:01 +0200

    refactor: consolidate h2 styles into H2Underline component

--- app/components/typography/h2-underline.tsx
+++ app/components/typography/h2-underline.tsx
@@ -1,7 +1,19 @@
+import classNames from "classnames";
+
 export default function H2Underline({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <h2 className="border-secondary w-full border-b pb-4">{children}</h2>;
+  return (
+    <h2
+      className={classNames(
+        "uppercase-tight text-sm leading-none font-semibold",
+        "text-primary border-secondary",
+        "w-full border-b pb-4",
+      )}
+    >
+      {children}
+    </h2>
+  );
 }
